0ce7358aae1063a52d6689ad2c95487ee6e86ec4
Diemlykt <143418329+Diemlykt@users.noreply.github.com>
2023-12-25 12:06:17 +0700

    Update get_dataset.py

--- train/get_dataset.py
+++ train/get_dataset.py
@@ -1,8 +1,16 @@
-from dataset import MRNetDataset, BRATSDataset, ADNIDataset, DUKEDataset, LIDCDataset, DEFAULTDataset
+from dataset import MRNetDataset, BRATSDataset, ADNIDataset, DUKEDataset, LIDCDataset, DEFAULTDataset, MRIDataset
 from torch.utils.data import WeightedRandomSampler
 
 
 def get_dataset(cfg):
+    if cfg.dataset.name == 'RAD_MR':
+        train_dataset = MRIDataset(
+            root_dir=cfg.dataset.root_dir, task=cfg.dataset.task, plane=cfg.dataset.plane, split='train')
+        val_dataset = MRIDataset(root_dir=cfg.dataset.root_dir,
+                                   task=cfg.dataset.task, plane=cfg.dataset.plane, split='valid')
+        sampler = WeightedRandomSampler(
+            weights=train_dataset.sample_weight, num_samples=len(train_dataset.sample_weight))
+        return train_dataset, val_dataset, sampler
     if cfg.dataset.name == 'MRNet':
         train_dataset = MRNetDataset(
             root_dir=cfg.dataset.root_dir, task=cfg.dataset.task, plane=cfg.dataset.plane, split='train')
